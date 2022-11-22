b47e3c5dc8d7e6ffe746bc5beab6a1a09a40ac38
Chris Bonet <christopher.bonet@gmail.com>
2022-11-21 22:30:07 -0500

    commit tree 560d986e3b9266ba25568b58cde1213d79b39c88

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    if (start >= 42)
+        return (start - 42);
+    else {
+        return (42 - start)
+    }
+};
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination >= start) 
+    return (destination - start) * 264;
+    else {
+        return (start - destination) * 264
+    };
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 0 && distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * .02; 
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+     } else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+}
